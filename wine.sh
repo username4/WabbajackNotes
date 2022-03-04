e4a9e7ab20f79db19a3cd545a51093227355e715
username4 <micsverau@web.de>
2022-03-04 19:16:38 +0100

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-WINEPREFIX="/home/muser/SSD2/Games/Proton/wine-mount" "./lutris-GE-Proton7-2-x86_64/bin/wine" /home/muser/SSD2/Games/Proton/Installers/dotnet-runtime-5.0.14-win-x64.exe
+WINEPREFIX="/home/muser/SSD2/Games/Proton/wine-mount" "./lutris-GE-Proton7-2-x86_64/bin/wine" 
